6742920fcf3d665d70dba6707dee4e1d23d83f50
wela-herveyyy <hervey.geralph@livro.systems>
2026-07-24 08:49:49 +0800

    feat: export employee schema and add company relations

--- packages/db/src/schema/firm/relations.ts
+++ packages/db/src/schema/firm/relations.ts
@@ -1,12 +1,16 @@
 import { relations } from "drizzle-orm/relations";
 import { user } from "../auth/tables";
 import { company, workspace } from "./tables";
+import { employee, deductionType, allowanceType } from "../employee/tables";
 
-export const companyRelations = relations(company, ({ one }) => ({
+export const companyRelations = relations(company, ({ one, many }) => ({
   createdByUser: one(user, {
     fields: [company.createdBy],
     references: [user.id],
   }),
+  employees: many(employee),
+  deductionTypes: many(deductionType),
+  allowanceTypes: many(allowanceType),
 }));
 
 export const workspaceRelations = relations(workspace, ({ one }) => ({
--- packages/db/src/schema/index.ts
+++ packages/db/src/schema/index.ts
@@ -2,3 +2,5 @@ export * from "./auth/tables";
 export * from "./auth/relations";
 export * from "./firm/tables";
 export * from "./firm/relations";
+export * from "./employee/tables";
+export * from "./employee/relations";
